daa3d9701af7cec6abab4bdfa3c14ddca2b1f173
Timothy Miller <miller.tim108@gmail.com>
2025-07-02 14:26:44 -0400

    terraform volume for data

--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -58,6 +58,41 @@ resource "azurerm_container_app" "basemap" {
       image  = "docker.io/maptiler/tileserver-gl-light:v5.3.1"
       cpu    = 0.25
       memory = "0.5Gi"
+
+      volume_mounts {
+        name = "data"
+        path = "/data"
+      }
     }
+
+    volume {
+      name         = "data"
+      storage_name = azurerm_container_app_environment_storage.basemap.name
+      storage_type = "AzureFile"
+    }
+
   }
+}
+
+resource "azurerm_storage_account" "content" {
+  name                     = "cartographycontent"
+  resource_group_name      = azurerm_resource_group.cartography.name
+  location                 = azurerm_resource_group.cartography.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "basemapcontent" {
+  name               = "basemapcontent"
+  storage_account_id = azurerm_storage_account.content.id
+  quota              = 5
+}
+
+resource "azurerm_container_app_environment_storage" "basemap" {
+  name                         = "basemapstorage"
+  container_app_environment_id = azurerm_container_app_environment.basemap.id
+  account_name                 = azurerm_storage_account.content.name
+  share_name                   = azurerm_storage_share.basemapcontent.name
+  access_key                   = azurerm_storage_account.content.primary_access_key
+  access_mode                  = "ReadOnly"
 }
